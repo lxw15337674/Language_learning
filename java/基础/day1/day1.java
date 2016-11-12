2c6ac982b1d7913f6fac5f145a61fc55b8159a99
lixiwang <404174262@qq.com>
2016-11-12 13:19:10 +0800

    add day1

--- java/基础/day1/day1.java
+++ java/基础/day1/day1.java
@@ -1,7 +1,8 @@
-class day1{
+
+public class day1{
 	public static void main(String[] args){
-		int i = 10;
-		byte b = i;
-		system.out.println(b);
+		int a = 10;
+		double b = 3;
+		System.out.println(a/b);
 	}
 }
---
+++ java/基础/day1/day2.java
@@ -0,0 +1,6 @@
+/**
+ * Created by 40417 on 2016/11/12 0012.
+ */
+public class day2 {
+
+}
