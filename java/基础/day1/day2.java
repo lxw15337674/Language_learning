044f2479496833190954bb76ded340baae9091b9
lixiwang <404174262@qq.com>
2016-11-15 17:20:00 +0800

    update

--- java/基础/day1/day2.java
+++ java/基础/day1/day2.java
@@ -2,5 +2,23 @@
  * Created by 40417 on 2016/11/12 0012.
  */
 public class day2 {
+    public static void main(String[] args) {
+        int a = 25;
+        String b = a>=18?"成年人":"未成年人";
+        System.out.println(b);
+
+        int c = 5 ;
+        do {
+            System.out.println('1');
+            c++;
+        }while(c<5);
+
+        for (int a1=0;a1<10;a1++){
+            System.out.println(a1);
+        }
+
+        System.out.println("hello哈哈123123123\rworld!");
+    }
+
 
 }
